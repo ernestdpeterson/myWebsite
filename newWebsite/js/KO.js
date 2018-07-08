35e30a792e201263ed863884cef4e08cbb75af58
Ernest D. Peterson <Myself@localhost.localdomain>
2018-07-08 15:45:41 -0500

    Finish contact modal

--- newWebsite/js/KO.js
+++ newWebsite/js/KO.js
@@ -7,12 +7,14 @@ var viewModel = {
     biography: ko.observable(false),
     schoolWork: ko.observable(false),
     codingExamples: ko.observable(false),
+    workHistory: ko.observable(false),
 
     displayControl: function(data, event) {
         this.greeting(false)
         this.biography(false)
         this.schoolWork(false)
         this.codingExamples(false)
+        this.workHistory(false)
         
         switch (event.target.id) {
         case 'greeting':
@@ -26,6 +28,9 @@ var viewModel = {
             break;
         case 'codingExamples':
             this.codingExamples(true)
+            break;
+        case 'workHistory':
+            this.workHistory(true)
             break;
         }
     }
--- newWebsite/js/main.js
+++ newWebsite/js/main.js
@@ -12,8 +12,7 @@ var bio = {
     },
     'Skills' : [
         ' HTML5', ' &emsp;CSS3', ' &emsp;JavaScript', ' &emsp;JQuery', ' &emsp;KnockoutJS', ' &emsp;MySQL', '&emsp;Sass', '&emsp;Git', '&emsp;Gulp', '&emsp;Bootstrap', ' &emsp;Computer_Repair_and_Maintainance'
-    ],
-    'pictureURL' : 'images/longAgo.jpg'
+    ]
 };
 
 var education = {
@@ -76,71 +75,69 @@ var education = {
         {
             'title' : 'Sass Basic Course',
             'school' : 'Treehouse',
-            'dates' : '4/2018',
+            'dates' : '4/2018-4/2018',
             'url' : 'https://teamtreehouse.com'
         }
     ]
 };
 
-var work = {
-    'jobs' : [
-        {
-            'employer' : 'Airgas',
-            'title' : 'Truck Driver',
-            'location' : 'Star, MS',
-            'dates' : '06/2008-current',
-            'description' : 'Loading, delivering and unloading liquid CO2 or Dry Ice.<br>Complying with all local, state and federal regulations.<br>Managing paperwork for load, Department of Transportation and company regulatory compliance.'
-        },
-        {
-            'employer' : 'Peterson Transportation',
-            'title' : 'Owner/Operator',
-            'location' : 'Seminary, MS',
-            'dates' : '04/2003-6/2008',
-            'description' : 'Owned and operated my trucking company.<br>Used QuickBooks and TurboTax software to balance my books and file my taxes.<br>Managed work acquisition and regulatory compliance.'
-        },
-        {
-            'employer' : 'Flying J',
-            'title' : 'Truck Driver',
-            'location' : 'Perl, MS',
-            'dates' : '10/1999-4/2003',
-            'description' : 'Loading, delivering and unloading fuel to Flying J truck stops.'
-        },
-        {
-            'employer' : 'Fleet Morris',
-            'title' : 'Truck Driver',
-            'location' : 'Collins, MS',
-            'dates' : '05/1997-10/1999',
-            'description' : 'Loading, delivering and unloading fuel to various gas stations.'
-        },
-        {
-            'employer' : 'Sanderson Farms',
-            'title' : 'Manager, Truck Driver',
-            'location' : 'Collins, MS',
-            'dates' : '11/1993-5/1997',
-            'description' : 'Managed a spray crew of 4.<br>Became a live haul driver to help establish the live haul devision.<br>Substituted for managers needing time of as needed.'
-        },
-        {
-            'employer' : 'T&L Trucking',
-            'title' : 'Truck Driver',
-            'location' : 'Ellisville, MS',
-            'dates' : '02/1991-11/1993',
-            'description' : 'Delivered parts from/to GM warehouses and plants.'
-        }
-    ]
-};
+var work = [
+    {
+        'employer' : 'Airgas',
+        'title' : 'Truck Driver',
+        'location' : 'Star, MS',
+        'dates' : '06/2008-current',
+        'description' : 'Loading, delivering and unloading liquid CO2 or Dry Ice.<br>Complying with all local, state and federal regulations.<br>Managing paperwork for load, Department of Transportation and company regulatory compliance.'
+    },
+    {
+        'employer' : 'Peterson Transportation',
+        'title' : 'Owner/Operator',
+        'location' : 'Seminary, MS',
+        'dates' : '04/2003-6/2008',
+        'description' : 'Owned and operated my trucking company.<br>Used QuickBooks and TurboTax software to balance my books and file my taxes.<br>Managed work acquisition and regulatory compliance.'
+    },
+    {
+        'employer' : 'Flying J',
+        'title' : 'Truck Driver',
+        'location' : 'Perl, MS',
+        'dates' : '10/1999-4/2003',
+        'description' : 'Loading, delivering and unloading fuel to Flying J truck stops.'
+    },
+    {
+        'employer' : 'Fleet Morris',
+        'title' : 'Truck Driver',
+        'location' : 'Collins, MS',
+        'dates' : '05/1997-10/1999',
+        'description' : 'Loading, delivering and unloading fuel to various gas stations.'
+    },
+    {
+        'employer' : 'Sanderson Farms',
+        'title' : 'Manager, Truck Driver',
+        'location' : 'Collins, MS',
+        'dates' : '11/1993-5/1997',
+        'description' : 'Managed a spray crew of 4.<br>Became a live haul driver to help establish the live haul devision.<br>Substituted for managers needing time off as needed.'
+    },
+    {
+        'employer' : 'T&L Trucking',
+        'title' : 'Truck Driver',
+        'location' : 'Ellisville, MS',
+        'dates' : '02/1991-11/1993',
+        'description' : 'Delivered parts from/to GM warehouses and plants.'
+    }
+];
 
 var carouselArray = [
     {
         'divClass': 'carousel-item active',
-        'divTitle': 'Simple Frogger Clone',
-        'clickPath': 'froggerClone/index.html'
+        'divTitle': 'Simple Frogger Clone ',
+        'clickPath': 'froggerClone/index.html',
         'imageSource': 'images/froggerClone.png',
         'imageaAlt': 'Screenshot of frogger clone game',
         'gitURL': 'https://github.com/ernestdpeterson/Frogger'
     },
     {
         'divClass': 'carousel-item',
-        'divTitle': 'Google Maps API with KnockoutJS',
+        'divTitle': 'Google Maps API with KnockoutJS ',
         'clickPath': 'googleMaps/ko.html',
         'imageSource': 'images/googleKO.png',
         'imageaAlt': 'Screenshot of a google map',
@@ -148,7 +145,7 @@ var carouselArray = [
     },
     {
         'divClass': 'carousel-item',
-        'divTitle': 'My Website',
+        'divTitle': 'My Website ',
         'clickPath': 'index.html',
         'imageSource': 'images/website.png',
         'imageaAlt': 'Screenshot of this website',
@@ -162,4 +159,4 @@ var carouselArray = [
     //     'imageaAlt': '',
     //     'gitURL': ''
     // }
-    ];
+];
